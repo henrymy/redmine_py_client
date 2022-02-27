06b500689e426500b7f13a111b15e5cca2182319
Zhang Peng <zphenrymy@gmail.com>
2022-02-27 11:53:03 +0000

    Implement add comment to tickets closed yesterday.

--- check_ticket_update.py
+++ check_ticket_update.py
@@ -23,18 +23,8 @@ config = utils.load_conf(LOCAL_PATH, CONFIG_FILE_NAME)
 REDMINE_URL = config['redmine']['url']
 API_KEY = config['redmine']['api_key']
 PARENT_PRJ_ID = config['redmine']['parent_prj_id']
-PRJ_KW = config['redmine']['project_kw']
-PRJ_NOT_KW1 = config['redmine']['project_not_kw1']
-DEV_MEMS = config['dev_members']
-MAIL_SERVER = config['mailserver']['name']
-PORT = config['mailserver']['port']
-FROM = config['address']['from']
-TO = config['address']['to']
-
 DEBUG = config['debug']['trace']
 if DEBUG:
-    MAIL_SERVER = 'localhost'
-    PORT = 8025
     import pdb
     pdb.set_trace()
 
@@ -51,13 +41,12 @@ parent_prj = redmine.get_prj_by_id(PARENT_PRJ_ID)
 sub_prj_list = redmine.get_sub_prj_list(parent_prj)
 
 # get all ticket updated last day
-print("Collecting all tickets changed yesterday from following projects...")
+print("Collecting all tickets changed to close yesterday from following projects...")
 updated_issues = []
 old_status_id = 'open'
 new_status_id = 'closed'
 yesterday = datetime.now() - timedelta(1)
 date_yesterday = datetime.strftime(yesterday, '%Y-%m-%d')
-date_yesterday = '2022-02-24'
 
 for prj in sub_prj_list:
     print(prj.name)
@@ -73,7 +62,11 @@ print("Finished collecting updated tickets.")
 print("Total updated ticket number: %s" % len(updated_issues))
 print("-" * 10)
 
-"""
-available properties of issue object:
-'assigned_to', 'attachments', 'author', 'changesets', 'children', 'created_on', 'custom_fields', 'description', 'done_ratio', 'id', 'journals', 'priority', 'project', 'relations', 'status', 'subject', 'time_entries', 'tracker', 'updated_on', 'watchers'
-"""
+# add content here as note_str
+note_str = ''
+for issue in updated_issues:
+    print("Add comment to ticket %s ..." % issue.id)
+    if not DEBUG:
+        redmine.reply_to_issue(issue, notes=note_str)
+    print("Operation suceeded.")
+
