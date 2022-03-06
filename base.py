752137b4fd608c0b290f0874489eb980a37669f0
Zhang Peng <zphenrymy@gmail.com>
2022-03-06 12:53:43 +0000

    implement generate_report function in base.py.

--- base.py
+++ base.py
@@ -9,7 +9,8 @@ import os
 import smtplib
 import yaml
 
-from redminelib import Redmine, exceptions
+from redminelib import Redmine
+from redminelib import exceptions as redmine_exceptions
 
 import utils
 
@@ -59,6 +60,10 @@ class RedmineAPIClient:
                 break
         return status
 
+    # get status obj by id 
+    def get_status_by_id(self, status_id):
+        return self.redmine.issue_status.get(status_id)
+
     # get issue detail by issue id
     def get_issue_by_id(self, issue_id):
         issue_obj = self.redmine.issue.get(issue_id, include=['journals'])
@@ -90,6 +95,25 @@ class RedmineAPIClient:
             project_id=project.id, status_id=status_id, updated_on=query_date_str)
         return issues
 
+    # get issues from list of projects with status and date interval
+    # return a list of issues id
+    def get_issues_from_projects_with_status_between_somedays(
+        self,
+        projects,
+        status_id,
+        start_date, end_date):
+        status_issues = []
+        for prj in projects:
+            prj_status_issues = self.get_issues_by_prj_and_status_between_somedays(
+                prj, status_id, start_date, end_date)
+            try:
+                for issue in prj_status_issues:
+                    status_issues.append(issue)
+            # not permitted to see tickes
+            except redmine_exceptions.ForbiddenError:
+                pass
+        return status_issues
+ 
     # get all user obj
     def get_all_user(self):
         all_users = self.redmine.user.all()
@@ -117,8 +141,8 @@ class RedmineAPIClient:
                 return custom_field
         return None
 
-    # get tickets working by certain user 
-    def if_working_by_user(self, issue, user):
+    # get tickets working by certain user_id 
+    def if_working_by_user(self, issue, user_id):
         # 1st: check assigned_to property
         if self.issue_assigned_to_user(issue, user):
             return True
@@ -130,56 +154,60 @@ class RedmineAPIClient:
     # get tickets working by certain user list 
     def if_working_by_user_list(self, issue, user_ids):
         # 1st: check assigned_to property
-        if self.issue_assigned_to_user_list(issue, user_ids):
-            return True
+        user_id_hit = self.issue_assigned_to_user_list(issue, user_ids)
+        if user_id_hit:
+            return user_id_hit
         # 2nd: if currently not assigned, check journals too
-        if self.issue_ever_assigned_to_user_list(issue, user_ids):
-            return True
+        user_id_hit = self.issue_ever_assigned_to_user_list(issue, user_ids)
+        if user_id_hit:
+            return user_id_hit
         return False
 
-    # check an issue is assigned to certain user
-    def issue_assigned_to_user(self, issue, user):
+    # check an issue is assigned to certain user_id
+    def issue_assigned_to_user(self, issue, user_id):
         assigned_to = getattr(issue, 'assigned_to', None)
         if not assigned_to:
             return False 
-        if assigned_to.id == user.id:
+        if assigned_to.id == user_id:
             return True
         return False 
         
     # check an issue is assigned to certain user list
     # input para1: issue obj
     # input para2: list of user ids
-    # return true after hit first user_id
+    # return the user_id after hit first user_id
     def issue_assigned_to_user_list(self, issue, user_ids):
         assigned_to = getattr(issue, 'assigned_to', None)
         if not assigned_to:
             return False 
         if assigned_to.id in user_ids:
-            return True
+            return assigned_to.id
         return False 
 
     # check an issue has ever been assigned_to certain user
     # contidion: new value of assigned_to_id property in journal.details == user.id
-    def issue_ever_assigned_to_user(self, issue, user):
+    def issue_ever_assigned_to_user(self, issue, user_id):
         for journal in issue.journals:
-            for record in journal.details:
-                try:
-                    if record['name'] == 'assigned_to_id' and int(record['new_value']) == user.id:
-                        return True
-                except KeyError:
-                    pass
+            check_this_id = journal.user.id
+            journal_notes = getattr(journal, 'notes', '')
+            try:
+                if check_this_id == user_id and journal_notes != '':
+                    return check_this_id
+            except redmine_exceptions.ResourceAttrError:
+                pass
         return False 
 
     # check an issue has ever been assigned_to certain user list
     # contidion: new value of assigned_to_id property in journal.details in user_ids
     def issue_ever_assigned_to_user_list(self, issue, user_ids):
         for journal in issue.journals:
-            for record in journal.details:
-                try:
-                    if record['name'] == 'assigned_to_id' and int(record['new_value']) in user_ids:
-                        return True
-                except KeyError:
-                    pass
+            check_this_id = journal.user.id
+            journal_notes = getattr(journal, 'notes', '')
+            try:
+                if check_this_id in user_ids and journal_notes != '':
+                    return check_this_id
+            except redmine_exceptions.ResourceAttrError:
+                pass
         return False 
 
     # get user obj by user name
@@ -199,3 +227,40 @@ class RedmineAPIClient:
         # reply to issue with specific note
         self.set_issue_property(issue, notes=notes)
 
+    # generate ticket report for some date interval
+    def generate_report(self, start_date, end_date, projects, status_id, user_id_list):
+        try:
+            status = self.get_status_by_id(status_id)
+            status_name = status.name 
+        except redmine_exceptions.ResourceNotFoundError:
+            status_name = status_id
+        status_issues = []
+        print("Collecting tickets from %s to %s ..." % (start_date, end_date))
+        status_issues = self.get_issues_from_projects_with_status_between_somedays(
+            projects,
+            status_id,
+            start_date,
+            end_date)
+        print("From %s to %s: Total %s ticket number is %d" % ( 
+            start_date, end_date, status_name, len(status_issues)))
+
+        # build a dict with user_id as keys to calculate number of working tickets
+        working_by_user={}
+        for user_id in user_id_list:
+            working_by_user[user_id] = 0
+
+        # loop all ticket and add to each user id
+        for issue in status_issues:
+            issue_obj = self.get_issue_by_id(issue.id)
+            print(issue)
+            print('Check if working by users in users_dict...')
+            check_this_id = self.if_working_by_user_list(issue, user_id_list)
+            if check_this_id:
+                working_by_user[check_this_id] += 1
+
+        print("Total %s tickets for each user:" % status_name)
+        print("-" * 10)
+        for user_id in user_id_list:
+            user = self.get_user_by_id(user_id)
+            print("%s %s: %s" % (user.lastname, user.firstname, working_by_user[user.id]))
+        print("-" * 10)
--- get_ticket_with_date.py
+++ get_ticket_with_date.py
@@ -23,18 +23,10 @@ config = utils.load_conf(LOCAL_PATH, CONFIG_FILE_NAME)
 REDMINE_URL = config['redmine']['url']
 API_KEY = config['redmine']['api_key']
 PARENT_PRJ_ID = config['redmine']['parent_prj_id']
-PRJ_KW = config['redmine']['project_kw']
-PRJ_NOT_KW1 = config['redmine']['project_not_kw1']
 DEV_MEMS = config['dev_members']
-MAIL_SERVER = config['mailserver']['name']
-PORT = config['mailserver']['port']
-FROM = config['address']['from']
-TO = config['address']['to']
 
 DEBUG = config['debug']['trace']
 if DEBUG:
-    MAIL_SERVER = 'localhost'
-    PORT = 8025
     import pdb
     pdb.set_trace()
 
@@ -48,50 +40,24 @@ else:
 # get current user who is using this api-key
 myself = redmine.get_current_user()
 
+# define dev members here
+dev_members = DEV_MEMS
+
 # get parent project
 parent_prj = redmine.get_prj_by_id(PARENT_PRJ_ID)
 # get tenant projects
 sub_prj_list = redmine.get_sub_prj_list(parent_prj)
 
-# get all issues from each project
-print("Collecting tickets from following projects...")
-open_issues = []
-closed_issues = []
+# build a list with user_id to calculate number of working tickets
+user_id_list = []
+for dev_mem in dev_members:
+    user_id_list.append(dev_mem['user_id'])
 end_date = datetime.now().strftime('%Y-%m-%d')
 last_30_days = datetime.now() - timedelta(30)
 start_date = last_30_days.strftime('%Y-%m-%d')
-for prj in sub_prj_list:
-    print(prj.name)
-    prj_open_issues = redmine.get_issues_by_prj_and_status_between_somedays(
-        prj,
-        'open',
-        start_date,
-        end_date)
-    prj_closed_issues = redmine.get_issues_by_prj_and_status_between_somedays(
-        prj,
-        'closed',
-        start_date,
-        end_date)
-    try:
-        for issue in prj_open_issues:
-            open_issues.append(issue)
-        for issue in prj_closed_issues:
-            closed_issues.append(issue)
-    # not permitted to see tickes
-    except redmine_exceptions.ForbiddenError:
-        pass
+status_id = 'closed'
+redmine.generate_report(start_date, end_date, sub_prj_list, status_id, user_id_list)
 
-print("Finished collecting tickets.")
-print("Total open ticket number: %s" % len(open_issues))
-print("Total closed ticket number: %s" % len(closed_issues))
-# define dev members here
-dev_members = DEV_MEMS
-"""
-dev_members = [
-    {'user_name': '染谷 諭司', 'user_id': '6354'},
-    {'user_name': '張 鵬', 'user_id': '5883'},
-    {'user_name': '内海 卓也', 'user_id': '1831'}
-]
 """
 # build a dict with user_id as keys to calculate number of working tickets
 working_by_user={}
@@ -101,39 +67,40 @@ for dev_mem in dev_members:
     user_obj = redmine.get_user_by_id(user_id)
     working_by_user[user_id] = 0
 print(working_by_user)
+# get all issues from each project
+print("Collecting tickets from all sub projects...")
+status_issues = []
+print("Collecting tickets last 30 days...")
+end_date = datetime.now().strftime('%Y-%m-%d')
+last_30_days = datetime.now() - timedelta(30)
+start_date = last_30_days.strftime('%Y-%m-%d')
+status_id = 'open'
+status_issues = redmine.get_issues_from_projects_with_status_between_somedays(
+    sub_prj_list,
+    'open',
+    start_date,
+    end_date)
+print("From %s to %s: Total %s ticket number is %d" % (
+    start_date, end_date, status_id, len(status_issues)))
+
+status_id = 'closed'
+closed_issues = redmine.get_issues_from_projects_with_status_between_somedays(
+    sub_prj_list,
+    'closed',
+    start_date,
+    end_date)
+print("From %s to %s: Total %s ticket number is %d" % (
+    start_date, end_date, status_id, len(status_issues)))
+
 # loop all ticket and add to each user id
-"""
-# use for test single ticket only
-test_issue_id = '83273'
-# get issue object
-test_issue_obj = redmine.get_issue_by_id(test_issue_id)
-test_issues = [test_issue_obj]
-for issue in test_issues:
-"""
 for issue in closed_issues:
     issue_obj = redmine.get_issue_by_id(issue.id)
     print(issue)
-    print('Check if assigned_to dev_members...')
-    check_this_id =''
-    # 1st: check assigned_to property
-    if getattr(issue_obj, 'assigned_to', False):
-        check_this_id = issue_obj.assigned_to.id
-        if check_this_id in working_by_user:
-            print("Catch One in assigned_to!")
-            print(check_this_id)
-            working_by_user[check_this_id] += 1
-            continue
-    # 2nd: if not assigned_to dev_member, check journals too
-    # contidion: edited by dev_mem and journal.note is not empty
-    print('Check if edited by dev_members...')
-    for journal in issue_obj.journals:
-        check_this_id = journal.user.id
-        journal_notes = getattr(journal, 'notes', '')
-        if check_this_id in working_by_user and journal_notes != '':
-            print("Catch One in issue journals!")
-            print(check_this_id)
-            working_by_user[check_this_id] += 1
-            break
+    print('Check if working by dev_members...')
+    check_this_id = redmine.if_working_by_user_list(issue, working_by_user.keys())
+    if check_this_id:
+        working_by_user[check_this_id] += 1
+        
 print("Total working tickets for each dev members:")
 print("-" * 10)
 for dev_mem in dev_members:
@@ -143,6 +110,7 @@ for dev_mem in dev_members:
     print("%s:%s" % (user_name, working_tickets))
 print("-" * 10)
 
+"""
 """
 available properties of issue object:
 'assigned_to', 'attachments', 'author', 'changesets', 'children', 'created_on', 'custom_fields', 'description', 'done_ratio', 'id', 'journals', 'priority', 'project', 'relations', 'status', 'subject', 'time_entries', 'tracker', 'updated_on', 'watchers'
